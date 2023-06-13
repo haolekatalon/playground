d416aa260c1959e83e646e5d7f90f66edb9bd9b9
Hao <91060875+haolekatalon@users.noreply.github.com>
2023-06-13 14:26:55 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,9 +18,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'haolekatalon', // Usually your GitHub org/user name.
+  projectName: 'playground', // Usually your repo name.
+  trailingSlash: false,
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
